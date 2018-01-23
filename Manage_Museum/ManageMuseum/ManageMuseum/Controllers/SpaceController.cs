b7ad08019b80f1be8c35d65fd73db7b38a60e8fd
jessicasFranco <jessicasfranco@gmail.com>
2018-01-23 16:54:08 +0000

    Map Stuff

---
+++ Manage_Museum/ManageMuseum/ManageMuseum/Controllers/SpaceController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ManageMuseum.Models;
+
+namespace ManageMuseum.Controllers
+{
+    public class SpaceController : Controller
+    {
+        // GET: Space
+        public ActionResult Map()
+        {
+         
+            return View();
+        }
+    }
+}
