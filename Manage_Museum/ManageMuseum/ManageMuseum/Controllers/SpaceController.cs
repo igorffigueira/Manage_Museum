85be4f64c471cec0094efe560c4808a14d405b5e
jessicasFranco <jessicasfranco@gmail.com>
2018-01-24 01:05:58 +0000

    Alterar sumofpieces in a room
    View Map atualizada para alterar o sumofpieces de uma sala

--- Manage_Museum/ManageMuseum/ManageMuseum/Controllers/SpaceController.cs
+++ Manage_Museum/ManageMuseum/ManageMuseum/Controllers/SpaceController.cs
@@ -9,11 +9,36 @@ namespace ManageMuseum.Controllers
 {
     public class SpaceController : Controller
     {
+        private OurContectDb db = new OurContectDb();
         // GET: Space
         public ActionResult Map()
         {
-         
+            var rooms = db.RoomMuseums.ToList();
+            ViewBag.RoomMuseums = new SelectList(rooms, "Name", "Name");
+            ViewBag.capacity = TempData["capacity"];
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Map(SpaceViewModel roomMuseum)
+        {
+            var rooms = db.RoomMuseums.ToList();
+            ViewBag.RoomMuseums = new SelectList(rooms, "Name", "Name");
+            var roomName = roomMuseum.RoomName;
+            var roomCapacity = roomMuseum.SumRoomArtPieces;
+            //var roomCapacity = db.RoomMuseums.Single(s=>s.Name == roomName);
+            //TempData["capacity"] = roomCapacity.SumRoomArtPieces.ToString();
+            var Room = db.RoomMuseums.Single(s=>s.Name == roomName);
+
+            Room.SumRoomArtPieces = roomCapacity;
+            
+            db.SaveChanges();
+            
+
+            return RedirectToAction("Map");
+
+
+
         }
     }
 }
---
+++ Manage_Museum/ManageMuseum/ManageMuseum/Models/SpaceViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ManageMuseum.Models
+{
+    public class SpaceViewModel
+    {
+        //[Required(ErrorMessage = "Room is Required")]
+        public string RoomName { get; set; }
+
+        [Required(ErrorMessage = "Capacity of a room is Required")]
+        [Range(1, 100, ErrorMessage = "The number must between 1 and 100")]
+        public int SumRoomArtPieces { get; set; }
+    }
+}
