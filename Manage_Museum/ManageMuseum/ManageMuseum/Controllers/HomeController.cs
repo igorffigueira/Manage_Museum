6b38bb6dcde4d900a8bd0ec06ccfd335456ba87e
IgorFigueira <igorffigueira@gmail.com>
2018-01-29 16:02:50 +0000

    Events algorithms correction

--- Manage_Museum/ManageMuseum/ManageMuseum/Controllers/HomeController.cs
+++ Manage_Museum/ManageMuseum/ManageMuseum/Controllers/HomeController.cs
@@ -15,6 +15,7 @@ namespace ManageMuseum.Controllers
         public ActionResult Index()
         {
             var now = DateTime.Now.Date;
+            var nowDay = now.Day;
             var eventTypeExhibitionState = db.EventTypes.Single(s => s.Name == "exposicao");
             var eventClosedState = db.EventStates.Single(s => s.Name == "encerrado");
             var eventExhibitiondState = db.EventStates.Single(s => s.Name == "exibicao");
@@ -29,7 +30,7 @@ namespace ManageMuseum.Controllers
 
             foreach (var event_ in eventsOnExhibitionEmptyArtPieces)
             {
-                if (event_.EventType.Name == "exposicao")
+                if (event_.EventType.Name == "exposicao" && event_.StartDate.Day < now.Day)
                 {
                     var getEventRooms = db.RoomMuseums.Include(d => d.Event).Where(d => d.Event.Id == event_.Id).ToList();
                     foreach (var rooms in getEventRooms)
@@ -45,7 +46,7 @@ namespace ManageMuseum.Controllers
             }
 
             // Defines past events as closed, and the rooms associated with those events are again free
-            var oldEventsOnExihibtion = db.Events.Include(d => d.EventState).Include(d => d.EventType).Where(d => d.EnDate < now && d.EventState.Name == "exibicao").ToList();
+            var oldEventsOnExihibtion = db.Events.Include(d => d.EventState).Include(d => d.EventType).Where(d => d.EnDate.Day < now.Day && d.EventState.Name == "exibicao").ToList();
             foreach (var _events in oldEventsOnExihibtion)
             {
                 if (_events.EventType.Name == "exposicao")
@@ -57,10 +58,12 @@ namespace ManageMuseum.Controllers
                         foreach (var piece in getArtPieces)
                         {
                             piece.ArtPieceState = artPieceStorageState;
+                            rooms.SumRoomArtPieces -= 1;
                             db.SaveChanges();
                         }
                         rooms.SpaceState = spaceFreeState;
                         rooms.Event = null;
+                        _events.SumArtPieces -= 1;
                         db.SaveChanges();
                     }
                     _events.EventState = eventClosedState;
@@ -81,7 +84,7 @@ namespace ManageMuseum.Controllers
             }
 
             // Defines events in exhibition, and the rooms associated with those events busy
-            var NewEventsNotInExihibtion = db.Events.Include(d => d.EventType).Include(d => d.EventState).Where(d => d.StartDate <= now && d.EventState.Name == "aceites").ToList();
+            var NewEventsNotInExihibtion = db.Events.Include(d => d.EventType).Include(d => d.EventState).Where(d => d.StartDate.Day <= now.Day && d.EventState.Name == "aceites").ToList();
             foreach (var _event in NewEventsNotInExihibtion) // Coloca todos os eventos que o StartDate é agora ou um pouco antes, e que ainda se encontram com o estado "aceites"
             {
                 if (_event.EventType.Name == "exposicao")
@@ -89,10 +92,12 @@ namespace ManageMuseum.Controllers
                     var getEventsRooms = db.RoomMuseums.Include(d => d.SpaceState).Include(d => d.Event).Where(d => d.Event.Id == _event.Id).ToList();
                     foreach (var _room_ in getEventsRooms) // coloca todas as salas associadas ao evento nas condicoes acima, com o estado de salas ocupado
                     {
-                        if (_event.SumArtPieces < 1)
+                        if (_event.SumArtPieces < 1 && _event.StartDate.Day < now.Day)
                         {
                             _room_.SpaceState = spaceFreeState;
                             _event.EventState = eventClosedState;
+                            _room_.SumRoomArtPieces = 0;
+                            _event.SumArtPieces = 0;
                             _room_.Event = null;
                             db.SaveChanges();
                         }
